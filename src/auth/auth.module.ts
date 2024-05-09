a596cf91765981de7c57ae22159ea1da92702c19
chucky-golden <chukschibyke92@gmail.com>
2024-05-09 06:44:20 +0100

    jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,27 +1,33 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt-strategy';
-import { UserSchema } from '../users/schema/user.schema';
-import { AdminSchema } from '../admin/schema/admin.schema';
+import { User, UserSchema } from 'src/users/schema/user.schema';
+import { Admin, AdminSchema } from 'src/admin/schema/admin.schema';
 
 @Module({
-    imports: [
-      PassportModule.register({ defaultStrategy: 'jwt' }),
-      JwtModule.registerAsync({
-        inject: [ConfigService],
-        useFactory: (config: ConfigService) => {
-          return {
-            secret: config.get<string>('JWT_SECRET'),
-            signOptions: {
-              expiresIn: config.get<string | number>('JWT_EXPIRE')
-            }
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: config.get<string | number>('JWT_EXPIRE')
           }
         }
-      })
-    ],
-    providers: [JwtStrategy, { provide: 'UserModel', useFactory: () => UserSchema }, { provide: 'AdminModel', useFactory: () => AdminSchema }],
-    exports: [JwtStrategy, PassportModule], 
+      }
+    }),
+    MongooseModule.forFeature([
+        { name: User.name, schema: UserSchema },
+        { name: Admin.name, schema: AdminSchema },
+    ]),
+  ],
+  controllers: [],
+  providers: [JwtStrategy],
+  exports: [JwtModule, JwtStrategy, PassportModule], // here we wxport module we want to use outside this app
 })
 export class AuthModule {}
