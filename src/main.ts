25a56f4e7a9e10025715fe23601245fe3359df7e
chucky-golden <chukschibyke92@gmail.com>
2024-05-10 05:34:34 +0100

    port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe())  
 
-  await app.listen(3000);
+  const config_service = new ConfigService()
+
+  const PORT = config_service.get<number>("PORT");
+
+  console.log(PORT);
+  
+
+  await app.listen(PORT);
 }
 bootstrap();
