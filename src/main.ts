9b840c5cab85782d073e7fe1367887fec970008c
chucky-golden <chukschibyke92@gmail.com>
2024-06-27 12:01:34 +0100

    changes

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*',
+  };
+
+  app.enableCors(corsOptions)
 
   app.useGlobalPipes(new ValidationPipe())  
 
